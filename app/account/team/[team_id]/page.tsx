7654dc27ef46d673fb17d1a5f59c03d1d18f6c84
hadiikaddour <hadyqdwr77@gmail.com>
2025-02-25 19:29:05 +0300

    removing cookies in team/id

--- app/account/team/[team_id]/page.tsx
+++ app/account/team/[team_id]/page.tsx
@@ -1,17 +1,12 @@
-import { cookies } from 'next/headers';
-import React from 'react';
+
 
 type Params = { team_id: string | number };
 
 const Page = async ({ params }: { params: Params }) => {
-  // استخدام await للحصول على الكوكيز
-  const cookieStore = await cookies();
-  const jwt = cookieStore.get('jwt')?.value || null;
 
   return (
     <div>
       <p>Team ID: {params.team_id}</p>
-      <p>JWT: {jwt ? jwt : "No JWT available"}</p>
     </div>
   );
 };
