ba5a683f2149ce9e4980260ab5cad67912dea85f
hadiikaddour <hadyqdwr77@gmail.com>
2025-02-27 00:27:28 +0300

    fixing shit

--- app/account/team/[team_id]/page.tsx
+++ app/account/team/[team_id]/page.tsx
@@ -1,6 +1,5 @@
 import AccountHeader from '@/app/components/account_components/AccountHeader';
 import InviteAndTableSection from '@/app/components/account_components/InviteAndTableSection';
-import { decodeJWT } from '@/lib/createJwt';
 import { cookies } from 'next/headers'
 
 export default async function Page({
