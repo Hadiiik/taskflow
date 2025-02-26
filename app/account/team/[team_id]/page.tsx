7d21cb0bda0635b57fca722b77a6eccb2b1983be
hadiikaddour <hadyqdwr77@gmail.com>
2025-02-27 00:28:36 +0300

    fixing

--- app/account/team/[team_id]/page.tsx
+++ app/account/team/[team_id]/page.tsx
@@ -1,5 +1,6 @@
 import AccountHeader from '@/app/components/account_components/AccountHeader';
 import InviteAndTableSection from '@/app/components/account_components/InviteAndTableSection';
+import { decodeJWT } from '@/lib/createJwt';
 import { cookies } from 'next/headers'
 
 export default async function Page({
@@ -13,7 +14,8 @@ export default async function Page({
     const cookieStore = await cookies();
     const jwt = cookieStore.get("jwt")?.value;
     // check if there is no jwt 
-    //const jwt_user = decodeJWT(jwt||""); 
+    const jwt_user = decodeJWT(jwt||"");
+    console.log(jwt_user) 
 
     return (
         <>
