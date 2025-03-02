c2f0dc89112aef2b593e4384b0d8e970653644b6
AbodZekra <hfgffjjdjfjgh@gmail.com>
2025-03-02 13:10:45 -0800

    Some modifications (4)

--- app/components/HorizntiolCard.tsx
+++ app/components/HorizntiolCard.tsx
@@ -1,4 +1,3 @@
-// components/Card.tsx
 import Image from 'next/image';
 import Link from 'next/link';
 import { FC } from 'react';
@@ -6,20 +5,20 @@ import { FC } from 'react';
 interface CardProps {
   title: string;
   details: string;
-  herf : string
+  herf: string;
 }
 
-const Card: FC<CardProps> = ({ title, details , herf }) => {
+const Card: FC<CardProps> = ({ title, details, herf }) => {
   return (
     <Link
       dir="rtl"
-      className=" mx-6  md:mx-24 my-4 p-4 bg-purple-600 border rounded-lg shadow-lg flex items-center justify-between"
-    href={herf}
+      className="mx-6 md:mx-24 my-4 p-4 bg-white border-2 border-purple-600 rounded-full shadow-lg flex items-center justify-between"
+      href={herf}
     >
       {/* محتوى البطاقة */}
       <div className="flex flex-col justify-center space-y-2">
-        <h2 className="text-xl font-semibold text-violet-100">{title}</h2>
-        <p className="text-gray-200">{details}</p>
+        <h2 className="text-xl font-semibold text-purple-600">{title}</h2>
+        <p className="text-purple-600">{details}</p>
       </div>
 
       {/* أيقونة الجدول */}
@@ -29,7 +28,7 @@ const Card: FC<CardProps> = ({ title, details , herf }) => {
           alt="جدول"
           width={30}
           height={30}
-          className="text-violet"
+          className="text-purple-600"
         />
       </div>
     </Link>
