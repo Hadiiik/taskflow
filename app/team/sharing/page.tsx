4bb20315b8c2cf69e59bca739f6134fe17d129c1
AbodZekra <hfgffjjdjfjgh@gmail.com>
2025-04-19 11:35:07 -0700

    ptz

---
+++ app/team/sharing/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const page= () => {
+  return (
+    <div className="text-center p-4">
+      <h1 className="text-2xl font-bold text-gray-800">زيارة الفريق</h1>
+    </div>
+  );
+};
+
+export default page;
